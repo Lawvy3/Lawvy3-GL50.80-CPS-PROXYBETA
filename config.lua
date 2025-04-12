c43fa45eafb84ff8213118b767cd3febdd5fd9b1
Lawvy3 <154064235+Lawvy3@users.noreply.github.com>
2025-04-12 20:36:50 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,1 +1,36 @@
-a
+return {
+
+  spam = {
+    text = "",
+    delay 5,
+    enable = 0
+  },
+
+  wrench = {
+    mode = 0 --none = 0, ban = 1, kick = 2 pull = 3
+  },
+
+  tax = {
+    game1 = 0,
+    game2 = 0,
+    minBet = 0,
+  },
+
+  autoJoin = 0, --none = 0, ban = 1, pull = 2
+
+  csn = {
+    cusNumMode = 0, --none = 0, reme = 1, qq = 2
+    dice = 1
+  },
+
+  hoster = {
+    posX1 = 0,
+    posY1 = 0,
+    posX2 = 0,
+    posY2 = 0
+  },
+
+  title = 0, --none = 0, dr = 1, legend = 2, mentor = 3, max = 4, g4g = 5
+
+  warn = ""
+}
